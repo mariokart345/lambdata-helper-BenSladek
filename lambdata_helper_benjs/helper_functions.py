019ce8cb6ece880b998999e95626b43157ba1e3d
mariokart345 <sladek.ben@gmail.com>
2021-03-19 01:49:53 -0400

    rename

--- lambdata_helper_benjs/helper_functions.py
+++ lambdata_helper_benjs/helper_functions.py
@@ -1,23 +1,30 @@
-def null_count(df):
-    null = df.isnull().sum().sum()
-    return print('Your dataset has {} null values.'.format(null))
+class Helper:
+    def __init__(self, data):
+        self.data = data
 
+    # Displays null count of dataframe
+    def null_count(self):
+        null = self.isnull().sum().sum()
+        return "Your dataset contains {} null values".format(null)
 
-def train_test_split(df, frac):
-    shape = df.shape[0]
-    train = df.iloc[:int((shape * (1 - frac)))]
-    test = df.iloc[int((shape * (1 - frac))):].reset_index(drop=True)
-    return train, test
+    # Randomizes order of dataframe and resets index
+    def randomize(self, seed):
+        randomized = self.sample(frac=1, random_state=seed).reset_index(drop=True)
+        return randomized
 
+    # Takes shape and splits on fractional value given - returns 2 dataframes
+    def train_test_split(self, frac):
+        shape = self.shape[0]
+        train = self.iloc[: int((shape * (1 - frac)))]
+        test = self.iloc[int((shape * (1 - frac))):].reset_index(drop=True)
+        return train, test
 
-def randomize(df, seed):
-    randomized = df.sample(frac=1, random_state=seed).reset_index(drop=True)
-    return randomized
-
-
-def rm_outlier(df):
-    q1, q3 = df.quantile(0.25), df.quantile(0.75)
-    iqr = q3 - q1
-    lower, upper = q1 - (1.5 * iqr), q3 + (1.5 * iqr)
-    df = df[~((df < lower) | (df > upper)).any(axis=1)]
-    return df
+    # Using only pandas methods, removes outliers via interquartile ranges
+    def rm_outlier(self):
+        q1 = self.quantile(0.25)
+        q3 = self.quantile(0.75)
+        iqr = q3 - q1
+        lower = q1 - (1.5 * iqr)
+        upper = q3 + (1.5 * iqr)
+        df = self[~((self < lower) | (self > upper)).any(axis=1)]
+        return df
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='lambdata_helper_benjs',
-    version='0.0.3',
+    version='0.0.4',
     description='A small project with datascience helper funcitons',
     url='https://github.com/mariokart345/lambdata-helper-benjs#installation',
     author='DSPT10',
     author_email='',
     keywords='pandas help, help, pandas, lambdata',
     packages=['lambdata_helper_benjs'],
-    python_requires='>=3.6',
-    install_requires=['pandas'],
+    python_requires='>=3',
+    install_requires='',
 )
